89a70a473aa00f7c2422ccca0bf4db2893c0e8ab
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2013-05-28 18:34:36 +0200

    Helper class

--- src/Pachanga/Helpers/Util.php
+++ src/Pachanga/Helpers/Util.php
@@ -30,4 +30,34 @@ class Util
 
     return $text;
   }
+
+  /**
+   * Generador aleatorio de descripciones.
+   *
+   * @return string
+   */
+  static public function getFrase()
+  {
+    $frases = array_flip(array(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Mauris ultricies nunc nec sapien tincidunt facilisis.',
+      'Nulla scelerisque blandit ligula eget hendrerit.',
+      'Sed malesuada, enim sit amet ultricies semper, elit leo lacinia massa, in tempus nisl ipsum quis libero.',
+      'Aliquam molestie neque non augue molestie bibendum.',
+      'Pellentesque ultricies erat ac lorem pharetra vulputate.',
+      'Donec dapibus blandit odio, in auctor turpis commodo ut.',
+      'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      'Nam rhoncus lorem sed libero hendrerit accumsan.',
+      'Maecenas non erat eu justo rutrum condimentum.',
+      'Suspendisse leo tortor, tempus in lacinia sit amet, varius eu urna.',
+      'Phasellus eu leo tellus, et accumsan libero.',
+      'Pellentesque fringilla ipsum nec justo tempus elementum.',
+      'Aliquam dapibus metus aliquam ante lacinia blandit.',
+      'Donec ornare lacus vitae dolor imperdiet vitae ultricies nibh congue.',
+    ));
+
+    $numeroFrases = rand(4, 7);
+
+    return implode("\n", array_rand($frases, $numeroFrases));
+  }
 }
