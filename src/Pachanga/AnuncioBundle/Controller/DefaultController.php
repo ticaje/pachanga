f7ed829ecdf05a14a49bbba5807b553c05647bac
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2013-05-30 18:13:07 +0200

    Create anuncio inde page

--- src/Pachanga/AnuncioBundle/Controller/DefaultController.php
+++ src/Pachanga/AnuncioBundle/Controller/DefaultController.php
@@ -10,4 +10,26 @@ class DefaultController extends Controller
     {
         return $this->render('AnuncioBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    /**
+     * Muestra la portada del sitio web
+     *
+     * @param string $ciudad El slug de la ciudad activa en la aplicación
+     */
+    public function portadaAction($ciudad)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $anuncios = $em->getRepository('AnuncioBundle:Anuncio')->findBy(array('actividad' => 2));
+
+        if (!$anuncios) {
+            throw $this->createNotFoundException('No se ha encontrado ninguna anuncio del día en la ciudad seleccionada');
+        }
+
+        $respuesta = $this->render('AnuncioBundle:Default:portada.html.twig', array(
+            'anuncios' => $anuncios
+        ));
+        $respuesta->setSharedMaxAge(60);
+
+        return $respuesta;
+    }
 }
